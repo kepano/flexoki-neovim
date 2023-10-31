f49f16130c675ac55e651eb4eb56021cee72d931
Jan Polak <polak@polakjan.com>
2023-10-31 18:09:02 +0100

    Import from old repo and small changes as discussed in #1

---
+++ lua/flexoki/init.lua
@@ -0,0 +1,19 @@
+local M = {}
+
+M.setup = function(opts)
+	local theme = opts.variant == 'light' and require('flexoki.theme-light') or require('flexoki.theme')
+
+	vim.cmd('hi clear')
+
+	vim.o.background = 'dark'
+	if vim.fn.exists('syntax_on') then
+		vim.cmd('syntax reset')
+	end
+
+	vim.o.termguicolors = true
+	vim.g.colors_name = 'flexoki'
+
+	theme.set_highlights()
+end
+
+return M
